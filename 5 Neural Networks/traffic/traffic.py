261845295f0461e4284047226219e4b61b98aadc
jsalaz1989 <jsalaz1989@gmail.com>
2020-05-21 18:39:16 +0200

    I think I finished 5 Neural Networks/traffic

--- 5 Neural Networks/traffic/traffic.py
+++ 5 Neural Networks/traffic/traffic.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
+
 import os
+# According to https://stackoverflow.com/questions/35911252/disable-tensorflow-debugging-information:
+# 0 = all messages are logged (default behavior)
+# 1 = INFO messages are not printed
+# 2 = INFO and WARNING messages are not printed
+# 3 = INFO, WARNING, and ERROR messages are not printed
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import sys
 import tensorflow as tf
 
@@ -9,8 +17,10 @@ from sklearn.model_selection import train_test_split
 EPOCHS = 10
 IMG_WIDTH = 30
 IMG_HEIGHT = 30
-NUM_CATEGORIES = 3 #43
+NUM_CATEGORIES = 43
 TEST_SIZE = 0.4
+
+
 
 
 def main():
@@ -62,10 +72,8 @@ def load_data(data_dir):
     labels = list()
 
     for root, dirs, files in os.walk(data_dir, topdown=False):
-        print(f'\n{root=}')
+        # print(f'\n{root=}')
         # print(f'{dirs=}')
-        # try: label = int(root[-1])        ;print(f'{label=}')
-        # except ValueError: pass
 
         try: label = int(os.path.split(root)[1])        ;print(f'{label=}')
         except ValueError: pass
@@ -78,7 +86,6 @@ def load_data(data_dir):
                 img = cv2.imread(os.path.join(root, name))   
                 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)   
                 img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
-
                 # print(f'{img=}')
                 # cv2.imshow('image',img)
                 # k = cv2.waitKey(0)
@@ -95,37 +102,37 @@ def get_model():
     `input_shape` of the first layer is `(IMG_WIDTH, IMG_HEIGHT, 3)`.
     The output layer should have `NUM_CATEGORIES` units, one for each category.
     """
-    # model = tf.keras.models.Sequential()
 
-    # model.add(tf.keras.layers.Dense(8, input_shape=(IMG_WIDTH, IMG_HEIGHT, 3), activation="relu"))
-
-    # model.add(tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax"))
-
-    # model.compile(
-    #     optimizer="adam",
-    #     loss="binary_crossentropy",
-    #     metrics=["accuracy"]
-    # )
-
-    # Create a convolutional neural network
     model = tf.keras.models.Sequential([
 
-        # Convolutional layer. Learn 32 filters using a 3x3 kernel
+        # Recommended by https://www.pyimagesearch.com/2018/12/31/keras-conv2d-and-convolutional-layers/:
+        #   - number of filters: 32, 64, or 128 for conv layer closest to input, 
+        #     but start with smaller and only increase if necessary. After playing around with the rest of the steps,
+        #     64 seems to bring me from 0.95 closer to 0.
+        #   - filter size: only go above (3,3) if input image larger than 128x128
+        #   - activation: seems fairly common to use relu
         tf.keras.layers.Conv2D(
-            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)
+            32, (3, 3), activation="relu", input_shape=(IMG_WIDTH, IMG_HEIGHT, 3)   
         ),
 
-        # Max-pooling layer, using 2x2 pool size
+        # Max-pooling with a 2x2 pool size seems to be common
         tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
 
         # Flatten units
         tf.keras.layers.Flatten(),
 
-        # Add a hidden layer with dropout
-        tf.keras.layers.Dense(128, activation="relu"),
-        tf.keras.layers.Dropout(0.5),
+        # According to https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
+        #   one hidden layer is usually enough for most problems 
+        #   and although the previous source says the number of nodes should be the mean between the number of input and out neurons,
+        #   according to http://www.faqs.org/faqs/ai-faq/neural-nets/part3/section-10.html
+        #   the number of nodes depends on various factors and recommend to simply try with different numbers
+        #   in this case it seems like the higher it is, the better?
+        #   Reducing the dropout seems to have the largest effect, but it risks overfitting (and it feels like cheating) 
+        tf.keras.layers.Dense(128*3, activation="relu"),
+        tf.keras.layers.Dropout(0.25),
 
-        # Add an output layer with output units for all 10 digits
+        # According to https://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
+        #   a classifer NN typically has in its output layer one node for each label if the activation is softmax
         tf.keras.layers.Dense(NUM_CATEGORIES, activation="softmax")
     ])
 
